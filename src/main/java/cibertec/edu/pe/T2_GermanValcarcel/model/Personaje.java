ad1de7865e2d9f94ce954b703d4323f8890938fa
germanval <germanvalcarcel_1500@hotmail.com>
2024-06-23 11:25:55 -0500

    pregunta3 inicio

--- src/main/java/cibertec/edu/pe/T2_GermanValcarcel/model/Personaje.java
+++ src/main/java/cibertec/edu/pe/T2_GermanValcarcel/model/Personaje.java
@@ -1,7 +1,19 @@
 package cibertec.edu.pe.T2_GermanValcarcel.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@NoArgsConstructor
 @Data
+@Table(name = "personaje")
 public class Personaje {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Integer idpersonaje;
+    private String nompersonaje;
+    private String apepersonaje;
+    private String fechnacpersonaje;
+
 }
--- src/main/java/cibertec/edu/pe/T2_GermanValcarcel/model/ProgramaTV.java
+++ src/main/java/cibertec/edu/pe/T2_GermanValcarcel/model/ProgramaTV.java
@@ -1,4 +1,24 @@
 package cibertec.edu.pe.T2_GermanValcarcel.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "programatv")
 public class ProgramaTV {
+    private Integer idprogramatv;
+    private String titulo;
+    private String resumen;
+    private Date fechainicio;
+    private Integer idpersonaje;
 }
