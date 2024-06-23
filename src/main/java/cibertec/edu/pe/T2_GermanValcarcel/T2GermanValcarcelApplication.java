7ea2ce5df2dd4502da6a000df115d1737b01d786
germanval <germanvalcarcel_1500@hotmail.com>
2024-06-23 09:04:06 -0500

    pregunta1 inicio

---
+++ src/main/java/cibertec/edu/pe/T2_GermanValcarcel/T2GermanValcarcelApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.T2_GermanValcarcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2GermanValcarcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2GermanValcarcelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/T2_GermanValcarcel/T2GermanValcarcelApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.T2_GermanValcarcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2GermanValcarcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
